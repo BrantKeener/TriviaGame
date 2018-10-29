99c27ca3ae7671f1a51a9d6a0d2fc37124bda71b
unknown <brant_keener@yahoo.com>
2018-10-29 17:37:33 -0600

    Update JS

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -2,53 +2,28 @@
 let triviaQuestions = [
     {
     count : 1,
-    question : 'bla',
-    answer : 'bla',
+    question : 'A top fuel dragster consumes how much Nitro-Methane every second?',
+    answer : '11.5 gallons',
     },
     {
     count : 2,
-    question : 'bla',
-    answer : 'bla',
+    question : 'What is the top speed reached by a top fuel dragster?',
+    answer : '336.57 MPH',
     },
     {
     count : 3,
-    question : 'bla',
-    answer : 'bla',
+    question : 'How many cubic feet per minute (CFM) of air is forced into the supercharger at full throttle?',
+    answer : '3000 CFM',
     },
     {
     count : 4,
-    question : 'bla',
-    answer : 'bla',
+    question : 'What is the difference between a supercharger and a turbocharger?',
+    answer : "A supercharger is driven by the engine directly, a turbocharger is driven by the engine's exhaust gas",
     },
     {
     count : 5,
-    question : 'bla',
-    answer : 'bla',
-    },
-    {
-    count : 6,
-    question : 'bla',
-    answer : 'bla',
-    },
-    {
-    count : 7,
-    question : 'bla',
-    answer : 'bla',
-    },
-    {
-    count : 8,
-    question : 'bla',
-    answer : 'bla',
-    },
-    {
-    count : 9,
-    question : 'bla',
-    answer : 'bla',
-    },
-    {
-    count : 10,
-    question : 'bla',
-    answer : 'bla',
+    question : 'A Top Fuel Dragster can exceed 300 MPH in approximately how many seconds?',
+    answer : '4.5',
     },
 ];
 
@@ -57,13 +32,13 @@ let growingWidth = 235;
 
 $(document).ready(function() {
 
-    $('#box1').click(function(){
-        growBoxStart();
+    $('.trivia_boxes').click(function(){
+        growBoxStart(event.target.id);
     });
     
-    function growBoxStart(){
-        $('#box1').addClass('selectedTrivia');
-        $('.selectedTrivia').css({'position':'absolute'});
+    function growBoxStart(e){
+        $('#' + e).addClass('selectedTrivia');
+        $('.selectedTrivia').css({'position':'absolute', 'top':'10px', 'left':'230px'});
         setInterval(boxGrow, 5);
     }
     function boxGrow(){
@@ -76,6 +51,14 @@ $(document).ready(function() {
         growingHeight += 3;
         growingWidth += 10;
     };
+
+    function populateQCards() {
+        for(let i = 1; i < triviaQuestions.length + 1; i++){
+            $('#box' + i).text('Question 1');
+        };
+    };
+
+    populateQCards();
 
 
 
