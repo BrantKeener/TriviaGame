8f96a65e0e4986ef2f77ea183b241e3ece4c51bd
unknown <brant_keener@yahoo.com>
2018-11-01 17:25:52 -0600

    lots of changes

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -3,46 +3,76 @@ let triviaQuestions = [
     {
     count : 1,
     question : 'A top fuel dragster consumes how much Nitro-Methane every second?',
-    answer : '11.5 gallons',
+    correctAnswer : '11.5 gallons',
+    answerChoices : ['1 gallon', '3.6 gallons', '11.5 gallons'],
     },
     {
     count : 2,
     question : 'What is the top speed reached by a top fuel dragster?',
-    answer : '336.57 MPH',
+    correctAnswer : '336.57 MPH',
+    answerChoices : ['154.87 MPH', '230.88 MPH', '336.57 MPH'],
     },
     {
     count : 3,
-    question : 'How many cubic feet per minute (CFM) of air is forced into the supercharger at full throttle?',
-    answer : '3000 CFM',
+    question : 'How many cubic feet per minute of air is forced into the supercharger at full throttle?',
+    correctAnswer : '3000 CFM',
+    answerChoices : ['1500 CFM', '50,000 CFM', '3000 CFM']
     },
     {
     count : 4,
     question : 'What is the difference between a supercharger and a turbocharger?',
-    answer : "A supercharger is driven by the engine directly, a turbocharger is driven by the engine's exhaust gas",
+    correctAnswer : "Turbochargers=exhaust gas driven, Superchargers=direct drive",
+    answerChoices : ['Superchargers are from Krypton', 'Turbocharges make airplanes fly', "Turbochargers=exhaust gas driven, Superchargers=direct drive"],
     },
     {
     count : 5,
     question : 'A Top Fuel Dragster can exceed 300 MPH in approximately how many seconds?',
-    answer : '4.5',
+    correctAnswer : '4.5 seconds',
+    answerChoices : ['1 second', '9 seconds', '4.5 seconds'],
     },
 ];
 
 let growingHeight = 150;
 let growingWidth = 235;
+let question = "";
+let corrects = 0;
+let incorrects = 0;
+let boxIDs = ['box1', 'box2', 'box3', 'box4', 'box5'];
+let qDivs = ['q1', 'q2', 'q3', 'q4', 'q5'];
 
 $(document).ready(function() {
 
-    $('.trivia_boxes').click(function(){
-        growBoxStart(event.target.id);
-        questionLabelRemove(event.target.id);
-        questionLabelMove(event.target.id);
-    });
+    function fullReset() {
+        corrects = 0;
+        incorrects = 0;
+        question = "";
+    }
+
+    function midGameReset() {
+        question = "";
+        growingHeight = 150;
+        growingWidth = 235;
+    };
     
+    function triviaBoxClickSet() {
+        $('.trivia_boxes').click(function(){
+            growBoxStart(event.target.id);
+            questionLabelRemove(event.target.id);
+            questionLabelMove(event.target.id);
+        });
+    };
+    
+    $('.choice_buttons').click(function() {
+        checkButton(event.target.id);
+        setTimeout(boxClear, 5000);
+    });
+
     function growBoxStart(e){
         $('#' + e).addClass('selectedTrivia');
         $('.selectedTrivia').css({'position':'absolute', 'top':'10px', 'left':'230px'});
         setInterval(boxGrow, 5);
-    }
+    };
+    
     function boxGrow(){
         if(growingHeight < 451){
             $('.selectedTrivia').css({'height': growingHeight + 'px'});
@@ -66,21 +96,33 @@ $(document).ready(function() {
 
     function questionLabelMove(e) {
         switch(e) {
+            case 'box1' : 
+                question = '0'
+                setTimeout(questionPost, 500)
+                break;
             case 'box2' :
                 $('.questLab2').addClass('questLab1')
                 $('.questLab2').removeClass('questLab2')
+                question = 1
+                setTimeout(questionPost, 500)
                 break;
             case 'box3' :
                 $('.questLab3').addClass('questLab1')
                 $('.questLab3').removeClass('questLab3')
+                question = 2
+                setTimeout(questionPost, 500)
                 break;
             case 'box4' :
                 $('.questLab4').addClass('questLab1')
                 $('.questLab4').removeClass('questLab4')
+                question = 3
+                setTimeout(questionPost, 500)
                 break;
             case 'box5' :
                 $('.questLab5').addClass('questLab1')
                 $('.questLab5').removeClass('questLab5')
+                question = 4
+                setTimeout(questionPost, 500)
                 break;
         };
     };
@@ -120,4 +162,86 @@ $(document).ready(function() {
         };
     };
 
+    function questionPost() {
+        $('.questLab1').append('<p>' + triviaQuestions[question].question + '</p>')
+        answerBuild(question);
+    };
+
+    function answerBuild(question) {
+        let answerArray = [];
+        for(let i = 0; i < 3; i++) {
+            let arrayPushing = Math.floor(Math.random() * triviaQuestions[question].answerChoices.length);
+            answerArray.push(triviaQuestions[question].answerChoices[arrayPushing]);
+            triviaQuestions[question].answerChoices.splice(arrayPushing, 1);
+        };
+        for(let i = 0; i < 3; i++) {
+            let classMachine = 'answer' + i;
+            $('.questLab1').append('<p class=' + classMachine + '>' + answerArray[i] + '</p>');
+        };
+    };
+
+    function checkButton(e) {
+        let answerText = document.getElementsByTagName('p');
+        if(e === 'redButton') {
+            if(answerText[2].innerHTML === triviaQuestions[question].correctAnswer) {
+                correctAnswerIncrease();
+            } else {
+                incorrectAnswerIncrease();
+            }
+        };
+        if(e === 'blueButton') {
+            if(answerText[3].innerHTML === triviaQuestions[question].correctAnswer) {
+                correctAnswerIncrease();
+            } else {
+                incorrectAnswerIncrease();
+            }
+        };
+        if(e === 'yellowButton') {
+            if(answerText[4].innerHTML === triviaQuestions[question].correctAnswer) {
+                correctAnswerIncrease();
+            } else {
+                incorrectAnswerIncrease();
+            }
+        };
+    };
+
+    function correctAnswerIncrease() {
+        $('.correct').text('Correct Answers: ' + ++corrects);
+        correctAnswerHighlight();
+        let correctSound = new Audio('assets/sounds/Ticket-machine-sound.mp3');
+        correctSound.play();
+        setTimeout(midGameReset, 1500);
+    };
+
+    function incorrectAnswerIncrease() {
+        $('.incorrect').text('Incorrect Answers: ' + ++incorrects);
+        correctAnswerHighlight();
+        let incorrectSound = new Audio('assets/sounds/Wrong-answer-sound-effect.mp3');
+        incorrectSound.play();
+        setTimeout(midGameReset, 1500);
+    };
+
+    function correctAnswerHighlight() {
+        let answerText = document.getElementsByTagName('p');
+        for(let i = 2; i < 5; i++) {
+            if(answerText[i].innerHTML !== triviaQuestions[question].correctAnswer) {
+                $(answerText[i]).css({'color' : 'red', 'opacity' : '0.2'});
+            }
+        };
+    };
+
+    function boxClear() {
+        qDivs.forEach(function(item) {
+            $('#' + item).empty(); 
+        });
+        qDivs.forEach(function(item) {
+            $('#' + item).append('<img class="trivia_boxes" src="assets/images/blue_box.jpg">');
+        });
+        for(let i = 0; i < 5; i++) {
+            let imageGrab = document.getElementsByTagName('img')
+            imageGrab[i].setAttribute('id', 'box' + (i + 1));
+        };
+        populateQCards();
+    };
+    triviaBoxClickSet();
 });
